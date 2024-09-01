3a419726732321e3540929659cbd370e85757e2c
John Cezar Ceneta <joceneta@my.cspc.edu.ph>
2024-09-01 18:15:10 +0800

    Add files via upload

---
+++ Ceneta_Lab2.java
@@ -0,0 +1,49 @@
+// Cylinder.java
+import java.util.Scanner;
+
+//Create Cylinder Class
+class Cylinder {
+    double radius, height, area;
+
+//Requesting input from the user
+    void getInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of cylinder: ");
+        radius = scanner.nextDouble();
+
+        System.out.print("Enter the height of cylinder: ");
+        height = scanner.nextDouble();
+
+        scanner.close();
+    }
+
+//Calculate the Area of the Cylinder
+    void calculateArea() {
+        area = 2 * Math.PI * Math.pow(radius, 2) + 2 * Math.PI * radius * height;
+    }
+}
+// CylinderVol.java
+class CylinderVol extends Cylinder {
+    double volume;
+
+//Calculate the Volume of the Cylinder
+    void calculateVolume() {
+        volume = Math.PI * Math.pow(radius, 2) * height;
+    }
+
+//Display results of the calculation of Area and Volume
+    void displayResults() {
+        System.out.println("Area of cylinder: " + area);
+        System.out.println("Volume of  cylinder: " + volume);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("**VOLUME AND AREA OF CYLINDER**");
+        CylinderVol cylinder = new CylinderVol();
+        cylinder.getInput();
+        cylinder.calculateArea();
+        cylinder.calculateVolume();
+        cylinder.displayResults();
+    }
+}
